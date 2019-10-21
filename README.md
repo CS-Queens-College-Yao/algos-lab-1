beefc450e603c52ed593a04d950e2e627024c622
CS-Queens-College-Yao <48690979+CS-Queens-College-Yao@users.noreply.github.com>
2019-10-21 14:27:50 -0400

    Initialize

---
+++ README.md
@@ -0,0 +1,10 @@
+# Labs
+
+The first lab is on Oct 22, 2019, touching on dynamic programming problems.
+First, instructions:
+* git fork this repository to your own github account, then clone it from there
+* work on the java files in your preferred editor + compiler
+* your code should have variables namedInThisStyle (snakecase), it should be clearly written and understandable, and it should have relevant comments where necessary.
+* put your writeups + drawings to the problems in a file titled YourName_DP_Writeup.pdf in this same directory
+* from your account, select "Create a pull request" where the base is this account's (CS-Queens-College-Yao) repository.
+* created a pull request from your fork to this original version, and then submit it via the Google form
---
+++ Robbery.java
@@ -0,0 +1,58 @@
+// You have a heist getaway sack with a capacity, and n items in front of you
+// with sizes and worths. Figure out the maximum value you could
+// get with the items.
+
+// You are encouraged to make helper functions!
+
+public class Robbery {
+
+	// Using DP: Get the maximum value with capacity C and n items
+	public int maximizeRobWorthRecur(
+		int capacity,
+		int[] sizes,
+		int[] worths
+	) {
+		// fill in here, change the return
+			return 2;
+	}
+
+	public int maximizeRobWorthBottomUp(
+		int capacity,
+		int[] sizes,
+		int[] worths
+	) {
+		// fill in here, change the return
+		return 2;
+	}
+
+/**
+* Bonus: figure out which items exactly
+* Takes in a DP DPTable
+* Returns an int array of the individual worths of the items you took
+*/
+	public int[] takeRobInventory(int[][] DPTable) {
+		// fill in here, change the return
+		return new int[DPTable.length];
+	}
+
+	public static void main(String[] args) {
+		Robbery r = new Robbery();
+		int bagCapacity = 40;
+		int[] itemSizes = {2, 25, 6, 13, 1, 15, 8, 5, 17, 4};
+		int[] itemWorths = {35, 120, 900, 344, 29, 64, 67, 95, 33, 10};
+
+		int maxWorthRecur = r.maximizeRobWorthRecur(bagCapacity, itemSizes, itemWorths);
+		System.out.println("Max worth of the bag: " + maxWorthRecur);
+		int maxWorthBottomUp = r.maximizeRobWorthBottomUp(bagCapacity, itemSizes, itemWorths);
+		System.out.println("Max worth of the bag: " + maxWorthBottomUp);
+
+		// Bonus: Fill in the helper method takeRobInventory that could help you
+		//figure out which items go into the bag that make it max worth. Feel free
+		//to change up the parameters and returned types + values of the helper
+		// methods above.
+		// int[] itemsPicked = r.takeRobInventory();
+		// System.out.println("The items we take are worth:")
+		// for (int i = 0; i < results.length; i++) {
+		// 	System.out.print(results[i] + " ");
+	}
+}
