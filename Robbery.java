b9177205da0771bb8706bf1359b51a8875180c61
Chuhui Chen <38293820+gonese@users.noreply.github.com>
2019-10-22 14:55:49 -0400

    Update Robbery.java

--- Robbery.java
+++ Robbery.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+
 // You have a heist getaway sack with a capacity, and n items in front of you
 // with sizes and worths. Figure out the maximum value you could
 // get with the items.
 
 // You are encouraged to make helper functions!
-
 public class Robbery {
 
 	// Using DP: Get the maximum value with capacity C and n items
@@ -21,8 +22,49 @@ public class Robbery {
 		int[] sizes,
 		int[] worths
 	) {
+		 ArrayList <Integer> fraction = new ArrayList<>();
+		 int value = 0;
+		 int lastpick = 0;
+		 int max = 0;
+	       for(int i=0; i<sizes.length;i++) {
+	    	   fraction.add(worths[i]/sizes[i]);
+	       }
+	       for(int i =0;i<fraction.size();i++) {
+	    	   System.out.print("indext "+i+" "+fraction.get(i)+" ");
+	       }
+	      while(capacity >= 0) {
+	    	  System.out.println("capacity :"+capacity);
+	    	  int sum=0;
+	     for (int i=0;i<fraction.size();i++) {
+	    	 if(fraction.get(i)!= -1)
+	    		 sum+= i;
+	     }
+	     if(sum == 0) {
+	    	 return value;
+	     }
+	    	  max = fraction.get(0);
+	      for(int i=0;i<fraction.size();i++) {
+	    	
+	    		   if(fraction.get(i) > max) {
+	    			   max = fraction.get(i);
+	    		  }
+	        }
+	       if(capacity - sizes[fraction.indexOf(max)]>=0) {
+	    	   System.out.println(fraction.indexOf(max));
+	    	   value += worths[fraction.indexOf(max)];
+	    	   capacity -= sizes[fraction.indexOf(max)];
+	    	   fraction.set(fraction.indexOf(max),-1);
+	    	   lastpick =fraction.indexOf(max);
+	    	   
+	       }
+	       else {
+	    	   fraction.set(fraction.indexOf(max),-1);
+	       }
+	      }
+         value-=worths[lastpick];
 		// fill in here, change the return
-		return 2;
+		
+		return value;
 	}
 
 /**
